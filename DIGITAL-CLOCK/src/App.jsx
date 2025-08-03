96a765efb35aaf6197c8f1e7e434c3430543b61f
Akinoso Mutmainnah <bolanlemutma@gmail.com>
2025-08-03 18:38:11 +0100

    first commit

---
+++ DIGITAL-CLOCK/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+import Digitalclock from './Digitalclock.jsx';
+
+const App = () => {
+  return (
+    <div>
+      <Digitalclock />
+    </div>
+  )
+}
+
+export default App
